b5f2c9bb9f7fb70c8e3f8f5d464bd2e1761bea10
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-08-15 20:57:30 -0500

    feat: dark mode

--- src/components/BaseTheme.tsx
+++ src/components/BaseTheme.tsx
@@ -6,7 +6,7 @@ export function BaseTheme(props: ThemeProps) {
     <Theme
       accentColor="indigo"
       grayColor="mauve"
-      appearance="light"
+      appearance="dark"
       {...props}
     />
   );
