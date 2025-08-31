9bbf743da01f5a8e26f5e14e6930442283bbfd4c
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-08-31 12:11:34 -0500

    feat: theming

--- src/components/BaseTheme.tsx
+++ src/components/BaseTheme.tsx
@@ -2,12 +2,5 @@ import { Theme, type ThemeProps } from "@radix-ui/themes";
 
 export function BaseTheme(props: ThemeProps) {
   // explicitly set light theme to set .light class to apply grays
-  return (
-    <Theme
-      accentColor="indigo"
-      grayColor="mauve"
-      appearance="dark"
-      {...props}
-    />
-  );
+  return <Theme accentColor="indigo" grayColor="mauve" {...props} />;
 }
