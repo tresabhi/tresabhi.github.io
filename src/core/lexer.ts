a45f47807774ab73fd4d4d622ba8cf24747acbd7
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2025-09-21 11:28:54 -0500

    make heading and descprioption optional

--- src/core/lexer.ts
+++ src/core/lexer.ts
@@ -1,7 +1,6 @@
 import { lexer as _lexer, use, type Tokens, type TokensList } from "marked";
 import { collectImages } from "./collectImages";
 import type { FrontMatter } from "./content";
-import { fail } from "./fail";
 import { git } from "./git";
 import { latexBlockExtension, latexInlineExtension } from "./latexExtensions";
 import { makeDate } from "./makeDate";
@@ -12,7 +11,7 @@ export interface Context {
 
   path: string;
   title: string;
-  description: string;
+  description?: string;
 
   images: Set<string>;
   created: Date;
@@ -23,18 +22,14 @@ use({ extensions: [latexBlockExtension, latexInlineExtension] });
 export async function lexer(file: string, path: string, content: string) {
   const tokens = _lexer(content);
 
-  const h0 =
+  const title =
     tokens.find(
       (token): token is Tokens.Heading =>
         token.type === "heading" && token.depth === 1
-    ) ?? fail("No heading found");
-  const p0 =
-    tokens.find(
-      (token): token is Tokens.Paragraph => token.type === "paragraph"
-    ) ?? fail("No description found");
-
-  const title = h0.text;
-  const description = p0.text;
+    )?.text ?? file;
+  const description = tokens.find(
+    (token): token is Tokens.Paragraph => token.type === "paragraph"
+  )?.text;
 
   const created = await git
     .log({ file, n: 1, "--diff-filter": "A" })
